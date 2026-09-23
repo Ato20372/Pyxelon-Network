541c1d1a302c0c7cd8a6c75e45d9fdb3c8142f1d
Ato20372 <atr20372@gmail.com>
2026-09-23 18:23:48 +0300

    Add files via upload

---
+++ base.js
@@ -0,0 +1,9 @@
+document.getElementById("ip-button").addEventListener("click", () => {
+    navigator.clipboard.writeText("pyxelon.hostivo.vip");
+});
+function openPopup() {
+    document.getElementById('myPopup').style.display = 'flex';
+}
+function closePopup() {
+    document.getElementById('myPopup').style.display = 'none';
+}
